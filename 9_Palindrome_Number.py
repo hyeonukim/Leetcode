692e239641c80a63ec564811558c0b4292c01fdf
ERIC <hka102@sfu.ca>
2022-05-07 19:51:41 -0700

    9.Palindrome Number answer

---
+++ 9_Palindrome_Number.py
@@ -0,0 +1,25 @@
+def answer(x):
+    result = 0
+    num = x
+    if (x < 0):
+        return False
+        
+    while (num > 0):
+        result *= 10
+        r = num % 10
+        result += r
+        num = num // 10
+
+    if (result == x):
+        return True
+    else:
+        return False
+123
+    
+if __name__ == "__main__":
+    x = int(input("Please input a number\n"))
+
+    if (answer(x) == True):
+        print("number is palindrome")
+    else:
+        print("number is NOT palindrome")
